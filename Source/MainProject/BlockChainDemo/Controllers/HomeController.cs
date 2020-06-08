f5e9635768187ec3e751181e8c88c5be2fb31d86
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-06-08 20:54:25 +0700

    edit view

--- Source/MainProject/BlockChainDemo/Controllers/HomeController.cs
+++ Source/MainProject/BlockChainDemo/Controllers/HomeController.cs
@@ -13,16 +13,29 @@ namespace BlockChainDemo.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult CreateWallet()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Account()
         {
-            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+        public ActionResult Transfer()
+        {
+            return View();
+        }
+
+        public ActionResult History()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Ví điện tử demo.";
 
             return View();
         }
