08d2130b252fafd5c922325b21468cadecb2c477
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-06-08 21:07:32 +0700

    add models (block,transaction,blockchain, base....)

---
+++ Source/MainProject/BlockChainDemo/Models/Transactions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace BlockChainDemo.Models
+{
+    public class Transactions
+    {
+        public string From { get; }
+        public string To { get; }
+        public double Amount { get; }
+        public Transactions(string from, string to, double amount)
+        {
+            From = from;
+            To = to;
+            Amount = amount;
+        }
+    }
+}
